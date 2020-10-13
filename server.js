9bd37029fc4b88591fa384c95cc79697ee786f8b
Garlic Fiend <suzannaakins@gmail.com>
2020-10-12 23:17:19 -0700

    / removed after app.get function

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
+app.get('api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
