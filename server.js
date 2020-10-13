f83ab4f6dffec27a5c96e9f21eb6acba5c6861e4
Garlic Fiend <suzannaakins@gmail.com>
2020-10-12 22:52:07 -0700

    port was hardcoded, changed to variable

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+    console.log(`API server now on port ${PORT}!`);
 });
